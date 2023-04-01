eaea952bac4ab74102388b8f2ac65f34f35b832e
nik <alan1992dzebis15rus@gmail.com>
2023-04-01 18:50:36 +0300

    урок 4 . арифмет. операции

---
+++ арифмет.действия.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().split())
+c = a * b
+d = ( a + b ) * 2
+print("Площадь = ", c)
+print("Периметр = ", d)
---
+++ задание 2.py
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+a, b, c, d, e = map(int, input().split())
+d =  d ** e
+c = d * 2 
+a = a - b  
+d = c // a 
+print(d)
